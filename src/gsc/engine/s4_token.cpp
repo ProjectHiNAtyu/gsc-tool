7ad4dfce091eadda8212d23f3283989b37b01c3e
HiNAtyu <projecthinatyu@gmail.com>
2024-10-21 23:56:53 +0900

    [add] Added some hashes of Vanguard (Credit. Sku-111)

--- src/gsc/engine/s4_token.cpp
+++ src/gsc/engine/s4_token.cpp
@@ -9,7 +9,11 @@ namespace xsk::gsc::s4
 {
 
 extern std::array<std::pair<u32, char const*>, token_count> const token_list
-{{   
+{{
+	{ 0x07A4 , "scripts/engine/utility" },
+	{ 0x2AA9 , "br_quests_Enabled" },
+	{ 0x2AAA , "br_queststashlocations" },
+	{ 0x6D9A , "GetStructArray" },
 }};
 
 } // namespace xsk::gsc::s4
